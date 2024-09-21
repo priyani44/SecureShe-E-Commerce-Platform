1cd36734d7a54cc53cc49ee888501a962b81cd6e
Megha Chakraborty <139854361+Megha-Chakraborty@users.noreply.github.com>
2024-09-21 13:45:58 +0530

    shop_section.jsx

---
+++ shop_section.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ShopSection = () => {
+  return (
+    <div className="shop-section">
+      <div className="box">
+        <h2>Beauty Products</h2>
+        <div className="boximage1"></div>
+        <p>See more</p>
+      </div>
+      <div className="box">
+        <h2>Skincare</h2>
+        <div className="box-image2"></div>
+        <p>See more</p>
+      </div>
+      <div className="box">
+        <h2>Haircare</h2>
+        <div className="box-image3"></div>
+        <p>See more</p>
+        </div>
+      <div className="box">
+        <h2>Fragrance</h2>
+        <div className="box-image4"></div>
+        <p>See more</p>
+      </div>
+      <div className="box">
+        <h2>Appliances</h2>
+        <div className="box-image5"></div>
+        <p>See more</p>
+      </div>
+    </div>
+  );
+};
+
+export default ShopSection;
